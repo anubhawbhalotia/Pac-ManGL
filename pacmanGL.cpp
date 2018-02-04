5fd6764b64bcc897ec64515024efd864925ec858
Anubhaw Bhalotia <bhalotia.anubhaw4@gmail.com>
2018-02-04 16:53:23 +0530

    window name updated

--- pacmanGL.cpp
+++ pacmanGL.cpp
@@ -3753,7 +3753,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window=glfwCreateWindow(WIDTH,HEIGHT,"SnakeGL",NULL,NULL);
+    GLFWwindow* window=glfwCreateWindow(WIDTH,HEIGHT,"Pac-ManGL",NULL,NULL);
     if(window==NULL)
     {
         cout<<"Failed to create GLFW window"<<endl;
